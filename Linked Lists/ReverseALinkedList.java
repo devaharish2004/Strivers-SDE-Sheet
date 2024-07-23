e2c91772afb877c8401a6920869838a717e8a99d
devaharish2004 <devaharishm@gmail.com>
2024-07-23 21:52:29 +0530

    Reverse a LL

---
+++ Linked Lists/ReverseALinkedList.java
@@ -0,0 +1,22 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+} 
+
+public class ReverseALinkedList {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode prevNode = null;
+        ListNode currNode = head;
+        while(currNode != null){
+            ListNode nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+        return prevNode;
+    }   
+}
